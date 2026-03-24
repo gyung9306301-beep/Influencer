924ba38e9a627cc14f9ec19a40aee19625845342
gyung9306301-beep <gyung9306301@gmail.com>
2026-03-24 16:39:27 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://yxudkqvhfrpclicoqjco.supabase.co';
+const supabaseAnonKey = '여기에_anon_publishable_key_sb_publishable__GO2bReJm9Yt5zfW0i2fkw_YCAKZM8y';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
